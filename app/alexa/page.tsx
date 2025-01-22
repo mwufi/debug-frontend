1f24fa46f5949e9f0708d4693db97393b6d096a7
Zen T <ztang230@gmail.com>
2025-01-21 18:41:14 -0600

    mini change

--- app/alexa/page.tsx
+++ app/alexa/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
                 <Header />
                 <div className="flex flex-col gap-9 h-full w-full items-center py-16 justify-center lg:w-4/5 mx-auto p-4">
                     <div className="flex flex-col items-start gap-1 w-full px-4 lg:w-4/5 max-w-[50rem]">
-                        <h1 className="text-foreground font-semibold text-3xl tracking-tight">
-                            <span className="text-primary">Good Morning</span>, Zen.
+                        <h1 className="font-semibold text-3xl tracking-tight">
+                            <span className="bg-gradient-to-r from-primary to-purple-500 bg-clip-text text-transparent">Good Morning</span>, Zen.
                         </h1>
                         <h1 className="text-muted-foreground text-3xl tracking-tight">
                             How can I help you today?
