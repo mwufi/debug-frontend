bc1887cdf2813da18741f942dc482a743132f00d
Zen T <ztang230@gmail.com>
2025-01-21 18:38:32 -0600

    d

--- app/alexa/page.tsx
+++ app/alexa/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@/components/header"
 import { BackgroundGradient } from "@/components/ui/background-gradient"
+import { AlexaForm } from "@/components/alexa/alexa-form"
 
 export default function Home() {
     return (
@@ -17,19 +18,7 @@ export default function Home() {
                         </h1>
                     </div>
                     <div className="w-full flex flex-col gap-2 items-center">
-                        <form className="w-full text-base flex flex-col gap-2 items-center justify-center relative z-10 mt-2">
-                            <div className="flex flex-row gap-2 justify-center w-full lg:w-4/5 relative">
-                                <div className="max-w-[50rem] relative w-full rounded-2xl sm:rounded-3xl duration-100 p-3 bg-input backdrop-blur-lg ring-2 ring-input-border ring-inset focus-within:ring-input-border-focus focus-within:bg-input-hover focus-within:shadow-[0px_2px_24px_2px_var(--input-glow)] hover:ring-input-border-focus hover:bg-input-hover">
-                                    <div className="relative">
-                                        <textarea
-                                            className="w-full py-2 px-3 bg-transparent focus:outline-none text-foreground align-bottom"
-                                            style={{ resize: "none", height: "66px" }}
-                                            placeholder="Generate an image for me"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                        <AlexaForm />
                     </div>
                 </div>
             </main>
